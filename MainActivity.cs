8d7347e032ad30cb1f57b5a6f2f2e74627a327ce
Ibrahim AlDekhail <idekhail@googlemail.com>
2023-10-20 15:13:29 +0300

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Plugin.Permissions;
+using Plugin.CurrentActivity;
+
+namespace XF_GoldenStore.Droid
+{
+    [Activity(Label = "XF_GoldenStore", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar; 
+            base.OnCreate(savedInstanceState); Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init (this, savedInstanceState);
+            LoadApplication(new App());
+
+
+
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults); 
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
